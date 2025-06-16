2307ffc02dc722dcb6b94a4df14c32e4c3b6d0a7
Vignesh babu A <vigneshofficial2020@gmail.com>
2025-06-16 15:25:47 +0530

    new probs updated

--- coding_challenges/src/javatpoint/Practice_2.java
+++ coding_challenges/src/javatpoint/Practice_2.java
@@ -1,10 +1,175 @@
 package javatpoint;
 
+import java.util.Collections;
+import java.util.List;
+
 public class Practice_2 {
 
     public static void main(String[] args) {
 
         printPatterns();
+        
+        fibonacci_series();
+
+        armstrong_number_problems();
+
+        factorial();
+
+        palindrome();
+
+        primeNumber();
+
+        findGCD();
+
+        findLargest();
+
+        // JavaTPoint String Programs
+        // JavaTPoint Array Programs
+        // JavaTPoint Searching and Sorting Programs
+    }
+
+    private static void findLargest() {
+        System.out.println("------- findLargest ------ ");
+        List<Integer> numList = List.of(545, 499, 8, 111, 550, 520, 45, 455, 500);
+        // biggest
+//        Integer maxNum = numList.stream().max(Integer::compare).get();
+        Integer maxNum = Collections.max(numList);
+        System.out.println("Max num is "+maxNum);
+
+        // second biggest
+        int biggest=Integer.MIN_VALUE, secondBigg=Integer.MIN_VALUE, num;
+        for(int i=0; i< numList.size(); i++){
+            num = numList.get(i);
+            if( num > biggest ) {
+                secondBigg = biggest;
+                biggest = num;
+            } else if( num > secondBigg && num < biggest ) secondBigg = num;
+        }
+        System.out.println("biggest = "+biggest+"  secondBigg = "+secondBigg);
+    }
+
+    private static void findGCD() {
+        System.out.println("------- findGCD ------ ");
+        int num1 = 8, num2 = 100, gcd=0;
+        for(int i=1; i<= Math.min(num1,num2); i++){
+            if ( num1 % i == 0 && num2 % i ==0 ) gcd = i;
+        }
+        System.out.println("GCD of "+num1+" & "+num2+" is "+gcd);
+    }
+
+    private static void primeNumber() {
+        System.out.println("------- primeNumber ------ ");
+        System.out.println("15 is "+( isPrime(15) ? " a ":" not a ")+" prime number");
+        generatePrimeNum();
+    }
+
+    private static void generatePrimeNum() {
+        StringBuilder sb = new StringBuilder();
+        for(int i=1; i<=1000; i++){
+            if (isPrime(i)) sb.append(" ").append(i);
+        }
+        System.out.println(sb);
+    }
+
+    private static boolean isPrime(int num) {
+        if (num < 2) return false;
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
+
+    private static void palindrome() {
+        System.out.println("------- palindrome ------ ");
+        System.out.println("121 is "+ ( isPalindrome(121) == true ? " a ":" not a " )+"palindrome");
+        generatePalindrome();
+    }
+
+    private static void generatePalindrome() {
+        StringBuilder sb = new StringBuilder();
+        for(int i=1; i<=1000; i++){
+            if ( isPalindrome(i) )  sb.append(" "+i );
+        }
+        System.out.println(sb);
+    }
+
+    private static boolean isPalindrome(int num) {
+        StringBuilder sb = new StringBuilder();
+        int r,temp=num;
+        do{
+            r=num%10;
+            sb.append(r);
+            num/= 10;
+        }while(num>0);
+
+        return String.valueOf(temp).equals(sb.toString());
+    }
+
+    private static void factorial() {
+        System.out.println("------- factorial ------ ");
+        findFactorial(5);
+        System.out.println("factorial of 5 is "+findFactorialRecursive(5));
+    }
+
+    private static int findFactorialRecursive(int num) {
+        if(num <= 1) return 1;
+        return num * findFactorialRecursive(num-1);
+    }
+
+    private static void findFactorial(int num) {
+        int fact=1;
+        for (int i=1; i<=num; i++){
+            fact *= i;
+        }
+        System.out.println("factorial of "+num+" is "+fact);
+    }
+
+    private static void armstrong_number_problems() {
+        System.out.println("------- armstrong_number_problems ------ ");
+        int num = 153;
+        System.out.println(num + (isArmstrong(num) ? " is" : " is not") + " an Armstrong number");
+        getArmstrongNumbers(10000);
+    }
+
+    private static void getArmstrongNumbers(int limit) {
+        System.out.println("Armstrong numbers for the limit : " + limit);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i < limit; i++) {
+            if (isArmstrong(i)) {
+                sb.append(i).append(" ");
+            }
+        }
+        System.out.println(sb);
+    }
+
+    private static boolean isArmstrong(int num) {
+        int digits = (int) Math.log10(num) + 1;
+        int temp = num, sum = 0;
+        while (temp > 0) {
+            int r = temp % 10;
+            sum += Math.pow(r, digits);
+            temp /= 10;
+        }
+        return sum == num;
+    }
+
+    private static void fibonacci_series() {
+        System.out.println("------- fibonacci_series ------ ");
+        int limit = 10, first = 0, second = 1, next;
+        if (limit < 1) {
+            System.out.println("Fibonacci series is not possible with limit less than 1.");
+            return;
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append(first);
+        for (int i = 1; i < limit; i++) {
+            sb.append(" ").append(second);
+            next = first + second;
+            first = second;
+            second = next;
+        }
+        System.out.println("Fibonacci series with limit = " + limit + ": ");
+        System.out.println(sb);
     }
 
     private static void printPatterns() {
